62226c1aa3fe542aa8bab9ab12323a907a1baee6
Ivo Petkov <ivo@ivopetkov.com>
2018-07-13 15:35:56 +0300

    Removed PSR autoloader from composer.json.

---
+++ autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Bear CMS standalone
+ * https://github.com/bearcms/standalone
+ * Copyright (c) Amplilabs Ltd.
+ * Free to use under the MIT license.
+ */
+
+$classes = [
+    'BearCMS\Standalone' => 'src/Standalone.php',
+];
+
+spl_autoload_register(function ($class) use ($classes) {
+    if (isset($classes[$class])) {
+        require __DIR__ . '/' . $classes[$class];
+    }
+}, true);
